9134d7318d48f70ceb6a419f9a63d3c6934b96e2
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-01-06 13:19:48 -0500

    trying to resolve an issue in heroku

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ const BlogPost = require('../models/blogpost');
 
 module.exports = async (req, res) => {
     const blogpost = await BlogPost.find({}).sort({_id: -1}).limit({limit: 1});
-    res.render('index', {
+    res.sendFile('index', {
         blogpost
     }); 
 };
